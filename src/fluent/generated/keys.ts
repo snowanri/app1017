ad5109ed0393b9a0864f9ee43720bfdebd1667a6
Admin1 WPT1 <test@example.com>
2024-11-13 16:59:26 -0800

    Added change test2.ts and pushing back to repo froim ide111x instance X v1.1.1

--- src/fluent/generated/keys.ts
+++ src/fluent/generated/keys.ts
@@ -13,6 +13,14 @@ declare global {
                         table: 'sys_script'
                         id: 'cb224f5e5ee7407c9304e851a6139643'
                     }
+                    src_server_script_js: {
+                        table: 'sys_module'
+                        id: 'fb709e365e734eab9cfd729934ed3ed9'
+                    }
+                    package_json: {
+                        table: 'sys_module'
+                        id: 'ccb22c97da6f46dfa1db2ca481bebd6b'
+                    }
                 }
             }
         }
